d859f35767bbb9f4912a1605c279d52b818ad628
lvxiaodong <lvxiaodong.web@gmali.com>
2023-09-09 17:44:47 +0800

    背离王选股

--- hlblw.py
+++ hlblw.py
@@ -3,65 +3,26 @@ import pandas as pd
 import talib as ta
 import datetime as dt
 import numpy as np
-import math
+from tqdm import tqdm
 from MyTT import *
 
-def IsNumber(value):
-    if not isinstance(value,(int,float)):
-        return False
-    if (math.isnan(value)):
-        return False
-    return True
-
-def CreateArray(count, value=float('nan')):
-        if count<=0:
-            return []
-        else:
-            return [value]*count
-
-def BACKSET(condition,n):
-        dataCount=len(condition)
-        if dataCount<=0:
-            return []
-
-        result=CreateArray(dataCount,0)   # 初始化0
-
-        for pos in range(dataCount):
-            if IsNumber(condition[pos]):
-                break
-
-        if pos==dataCount:
-            return result
-
-        num=min(dataCount-pos,max(n,1))
-        for i in range(dataCount-1, -1,-1):
-            value=condition[i]
-            if IsNumber(value) and value:
-                for j in range(i, i-num,-1):
-                    result[j]=1
-
-        if condition[i]:
-            for j in range(i, pos+1):
-                result[j]=1
-
-        return result
-
-pd.set_option('display.max_rows', None)
-pd.set_option('display.max_columns', None)
-
-start = dt.datetime(2022, 1, 1).strftime("%Y%m%d")
-now = dt.datetime.now().strftime("%Y%m%d")
+start_date = dt.datetime(2022, 1, 1).strftime("%Y%m%d")
+end_date = dt.datetime(2023, 8, 25).strftime("%Y%m%d")
+# end_date = dt.datetime.now().strftime("%Y%m%d")
 
 filepath = "A.csv"
 stocklist = pd.read_csv(filepath, dtype=str, engine="python")
 stock_codes = stocklist['code'].values
 
+progress_bar = tqdm(total=len(stock_codes))
+
 gold_cross_stocks=[]
 for code in stock_codes:
+    progress_bar.update(1)
     try:
         # 获取日线数据
-        stock_data = ak.stock_zh_a_hist(symbol=code, start_date=start, end_date=now, period="daily", adjust="qfq")
-        CLOSE = stock_data["收盘"]
+        stock_data = ak.stock_zh_a_hist(symbol=code, start_date=start_date, end_date=end_date, period="daily", adjust="qfq")
+        CLOSE = stock_data['收盘']
 
         # 计算指标
         A1 = FORCAST(EMA(CLOSE, 5), 6)
@@ -72,27 +33,23 @@ for code in stock_codes:
         B = A1+A2+A3+A4-4*A5
         TOWERC = EMA(B, 2)
 
-
         FLAG = TOWERC >= REF(TOWERC, 1)
         FLAG1 = pd.Series(REF(FLAG, 1)).bfill()
         COND1 = FLAG & (~FLAG1)
         PERIOD_COND1 = BARSLAST(REF(COND1,1));
+        COND2 = (CLOSE.iat[-1] < REF(CLOSE, PERIOD_COND1[-1] + 1)[-1]) & (TOWERC[-1] > REF(TOWERC, PERIOD_COND1[-1] + 1)[-1])
         
-        PERIOD_COND1 = pd.Series(PERIOD_COND1)
-        COND2=pd.Series(CLOSE<REF(CLOSE,PERIOD_COND1.iat[-1]+1)) & pd.Series(TOWERC>REF(TOWERC,PERIOD_COND1.iat[-1]+1))
-        COND3=COND1 & COND2;
-
-        AA1=BACKSET(COND3,PERIOD_COND1.iat[-1]+2)
-        BB1=FILTER(AA1,PERIOD_COND1.iat[-1]+1)
-
-        
-        if BB1[-1]:
-            print('找到了----'+code)
+        if COND1.iat[-1] & COND2:
+            print('找到了背离----'+code+'----')
             gold_cross_stocks.append(code)
-        else:
-            print(code)
     except Exception as e:
-        print("发生了异常:", e)
+        # 获取异常信息
+        exception_type = type(e).__name__  # 异常类型的名称
+        exception_message = str(e)  # 异常的具体描述信息
+
+        # 打印异常信息
+        print("异常类型:", exception_type)
+        print("异常信息:", exception_message)
 
 file_path = "hlblw.txt"
 with open(file_path, "w") as file:
@@ -100,3 +57,5 @@ with open(file_path, "w") as file:
     for code in gold_cross_stocks:
         line = str(code) + "\n"
         file.write(line)
+
+progress_bar.close()
