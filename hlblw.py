da15dd94ebbfb9a7f79c2ae05167c9295142726a
lvxiaodong <lvxiaodong.web@gmali.com>
2023-09-09 19:30:12 +0800

    背离王

--- hlblw.py
+++ hlblw.py
@@ -1,61 +1,72 @@
-import akshare as ak
+import akshare as ak 
 import pandas as pd
 import talib as ta
-import datetime as dt
 import numpy as np
+from datetime import datetime
 from tqdm import tqdm
-from MyTT import *
+from MyTT import EMA, REF, FORCAST, BARSLAST
 
-start_date = dt.datetime(2022, 1, 1).strftime("%Y%m%d")
-end_date = dt.datetime(2023, 8, 25).strftime("%Y%m%d")
-# end_date = dt.datetime.now().strftime("%Y%m%d")
+# 是否是分钟周期类型
+def is_min_period_type(period):
+    return period in ['1','5','15','30','60']
 
+period = '30'  #周期可选参数： 1 5 15 30 60 daily weekly monthly 
+
+# 设置查询数据的开始时间，结束时间
+start_date = '2021-01-01 00:00:00'  #分钟周期格式
+end_date = '2023-8-30 15:00:00'
+# end_date = dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+##兼容开始时间、结束时间
+if ~is_min_period_type(period):
+    start_date = datetime.strptime(start_date, "%Y-%m-%d %H:%M:%S")
+    start_date = start_date.strftime("%Y%m%d")
+    end_date = datetime.strptime(end_date, "%Y-%m-%d %H:%M:%S")
+    end_date = end_date.strftime("%Y%m%d")
+
+# 获取股票代码
 filepath = "A.csv"
-stocklist = pd.read_csv(filepath, dtype=str, engine="python")
-stock_codes = stocklist['code'].values
+stock_codes = pd.read_csv(filepath, dtype=str, engine="python")['code'].values
 
-progress_bar = tqdm(total=len(stock_codes))
+# 创建DataFrame存储结果
+results = pd.DataFrame(columns=['code']) 
 
-gold_cross_stocks=[]
-for code in stock_codes:
-    progress_bar.update(1)
-    try:
-        # 获取日线数据
-        stock_data = ak.stock_zh_a_hist(symbol=code, start_date=start_date, end_date=end_date, period="daily", adjust="qfq")
-        CLOSE = stock_data['收盘']
+print(start_date)
+with tqdm(total=len(stock_codes)) as progress_bar:
+    # 遍历查找股票
+    for code in stock_codes:
+        progress_bar.update(1)
+        try:
+            # 获取数据
+            if is_min_period_type(period):
+                df = ak.stock_zh_a_hist_min_em(symbol=code, start_date=start_date, end_date=end_date, period=period, adjust="qfq")
+            else:
+                df = ak.stock_zh_a_hist(symbol=code, start_date=start_date, end_date=end_date, period=period, adjust="qfq")
+            
+            # 向量化计算指标
+            CLOSE = df['收盘']
+            A1 = FORCAST(EMA(CLOSE, 5), 6)
+            A2 = FORCAST(EMA(CLOSE, 8), 6)
+            A3 = FORCAST(EMA(CLOSE, 11), 6)
+            A4 = FORCAST(EMA(CLOSE, 14), 6)
+            A5 = FORCAST(EMA(CLOSE, 17), 6)
+            B = A1+A2+A3+A4-4*A5
+            TOWERC = EMA(B, 2)
+            
+            FLAG = TOWERC >= REF(TOWERC, 1)
+            FLAG1 = pd.Series(REF(FLAG, 1)).bfill()
+            COND1 = FLAG & (~FLAG1)
+            PERIOD_COND1 = BARSLAST(REF(COND1,1));
+            COND2 = (CLOSE.iat[-1] < REF(CLOSE, PERIOD_COND1[-1] + 1)[-1]) & (TOWERC[-1] > REF(TOWERC, PERIOD_COND1[-1] + 1)[-1])
+            
 
-        # 计算指标
-        A1 = FORCAST(EMA(CLOSE, 5), 6)
-        A2 = FORCAST(EMA(CLOSE, 8), 6)
-        A3 = FORCAST(EMA(CLOSE, 11), 6)
-        A4 = FORCAST(EMA(CLOSE, 14), 6)
-        A5 = FORCAST(EMA(CLOSE, 17), 6)
-        B = A1+A2+A3+A4-4*A5
-        TOWERC = EMA(B, 2)
-
-        FLAG = TOWERC >= REF(TOWERC, 1)
-        FLAG1 = pd.Series(REF(FLAG, 1)).bfill()
-        COND1 = FLAG & (~FLAG1)
-        PERIOD_COND1 = BARSLAST(REF(COND1,1));
-        COND2 = (CLOSE.iat[-1] < REF(CLOSE, PERIOD_COND1[-1] + 1)[-1]) & (TOWERC[-1] > REF(TOWERC, PERIOD_COND1[-1] + 1)[-1])
-        
-        if COND1.iat[-1] & COND2:
-            print('找到了背离----'+code+'----')
-            gold_cross_stocks.append(code)
-    except Exception as e:
-        # 获取异常信息
-        exception_type = type(e).__name__  # 异常类型的名称
-        exception_message = str(e)  # 异常的具体描述信息
-
-        # 打印异常信息
-        print("异常类型:", exception_type)
-        print("异常信息:", exception_message)
-
-file_path = "hlblw.txt"
-with open(file_path, "w") as file:
-    file.write('弘历背离王选股结果:\n')
-    for code in gold_cross_stocks:
-        line = str(code) + "\n"
-        file.write(line)
-
-progress_bar.close()
+            # 记录结果
+            if COND1.iat[-1] & COND2:
+                results.loc[len(results.index)] = [code]
+                print('找到了背离----'+code+'----')
+                
+        except Exception as e:
+            print(e)
+            
+# 一次性写入文件            
+results.to_csv('hlblw.csv', index=False)
